57d76557b5426328a0e99a5718497193ca3ffc77
ShershnewAlexandr <3000cfyz@gmail.com>
2017-03-26 16:49:43 +0700

    dfg

--- game.js
+++ game.js
@@ -9,19 +9,29 @@ var cam = pjs.camera;
 var key = pjs.keyControl.initKeyControl();
 // --- всякое для движка и удобства
 
-var text;
-  VK.init(function() { 
-        console.log("yra zarobotolo");
-        VK.api("users.get",{"fields":"photo_50"},function(data){
-            text = game.newTextObject({
-                x: 10, y: 10,
-                fillColor: "#000000",
-                text: data.response[0].first_name 
-            })
-        });
-     }, function() { 
-        console.log("lol");
-     }, '5.63'); 
+var text = game.newTextObject( { 
+  x : 100, 
+  y : 100, 
+  text : "Привет, мир!", 
+  size : 20, 
+  padding : 10, 
+  color : "#000000", 
+  fillColor : "#FBFE6F", 
+  strokeColor : "#DA4848", 
+  strokeWidth : 2, 
+});;
+  // VK.init(function() { 
+  //       console.log("yra zarobotolo");
+  //       VK.api("users.get",{"fields":"photo_50"},function(data){
+  //           text = game.newTextObject({
+  //               x: 10, y: 10,
+  //               fillColor: "#000000",
+  //               text: data.response[0].first_name 
+  //           });
+  //       });
+  //    }, function() { 
+  //       console.log("lol");
+  //    }, '5.63'); 
 
 
 var lp1 = function(){
