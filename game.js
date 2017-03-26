04ef1a2faea0d74f1974c75203930949bf42112d
ShershnewAlexandr <3000cfyz@gmail.com>
2017-03-26 17:10:15 +0700

    asd

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 var pjs = new PointJS("2d",600,400,{
-    backgroundColor : "#ccc"
+	backgroundColor : "#ccc"
 });
 
 var game = pjs.game;
@@ -10,32 +10,32 @@ var key = pjs.keyControl.initKeyControl();
 // --- всякое для движка и удобства
 
 var text;
-  VK.init(function() { 
-        console.log("yra zarobotolo");
-        VK.api("users.get",{"fields":"photo_50"},function(data){
-            console.log("yra");
-            console.log(data);
-            text = game.newTextObject( { 
-  x : 100, 
-  y : 100, 
-  text : "Привет, мир!", 
-  size : 20, 
-  padding : 10, 
-  color : "#000000", 
-  fillColor : "#FBFE6F", 
-  strokeColor : "#DA4848", 
-  strokeWidth : 2, 
-});
-        });
-     }, function() { 
-        console.log("lol");
-     }, '5.63'); 
-
 
 var lp1 = function(){
-    game.clear();
-    text.draw();
+	game.clear();
+	text.draw();
 }
 
-  game.newLoop("lp1",lp1);
-  game.startLoop("lp1");
+	game.newLoop("lp1",lp1);
+
+	VK.init(function() { 
+		console.log("yra zarobotolo");
+		VK.api("users.get",{"fields":"photo_50"},function(data){
+			console.log("yra");
+			console.log(data);
+			text = game.newTextObject( { 
+			x : 100, 
+			y : 100, 
+			text : "Привет, мир!", 
+			size : 20, 
+			padding : 10, 
+			color : "#000000", 
+			fillColor : "#FBFE6F", 
+			strokeColor : "#DA4848", 
+			strokeWidth : 2, 
+});
+			game.startLoop("lp1");
+		});
+	 }, function() { 
+		console.log("lol");
+	 }, '5.63'); 
