472ea08ed5bb9d3528a6595983f5a7dfffa38f3b
ShershnewAlexandr <3000cfyz@gmail.com>
2017-03-26 16:35:54 +0700

    ert

--- game.js
+++ game.js
@@ -1,3 +1,16 @@
+var pjs = new PointJS("2d",600,400,{
+    backgroundColor : "#ccc"
+});
+
+var game = pjs.game;
+var math = pjs.math;
+var vector = pjs.vector;
+var cam = pjs.camera;
+var key = pjs.keyControl.initKeyControl();
+// --- всякое для движка и удобства
+
+
+
   VK.init(function() { 
         console.log("yra zarobotolo");
         VK.api("users.get",{"fields":"photo_50"},function(data){
@@ -6,3 +19,10 @@
      }, function() { 
         console.log("lol");
      }, '5.63'); 
+
+var lp1 = function(){
+    console.log(7);
+}
+
+  game.newLoop("lp1",lp1);
+  game.startLoop("lp1");
